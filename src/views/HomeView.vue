aad81dffcb98fb34efc37b180b421ab662f03b75
Meleneth <meleneth@gmail.com>
2024-06-13 10:15:42 +0000

    [feature] first pass at inventory, and fishing is a thing that happens

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,11 +9,24 @@ const skills = ref([
   { name: 'Cooking', level: 1, maxlevel: 99, description: "Cook raw food, increasing the amount healed"}
 ])
 
+const inventory = ref({})
+
 const items = ref([
-  {name: 'Raw Salmon', xp: {fishing: 5}, description: 'a tasty fish'},
-  {name: 'Raw Minnow', xp: {fishing: 1}, description: 'barely worth catching'},
-  {name: 'Raw Shrimp', xp: {fishing: 3}, description: 'delicious with butter'}
+  {name: 'Raw Salmon', xp: {fishing: 5}, description: 'a tasty fish', ticks: 10},
+  {name: 'Raw Minnow', xp: {fishing: 1}, description: 'barely worth catching', ticks: 4},
+  {name: 'Raw Shrimp', xp: {fishing: 3}, description: 'delicious with butter', ticks: 7}
 ]);
+
+var num_ticks = ref(0);
+const ticktimer = setInterval(() => {
+  num_ticks.value = num_ticks.value + 1;
+  if (num_ticks.value > current_skill_target.ticks) {
+    num_ticks.value -= current_skill_target.ticks
+    inventory.value[current_skill_target.name] = (inventory.value[current_skill_target.name] || 0) + 1
+  }
+}, 100);
+
+const current_skill_target = items.value[0]
 
 </script>
 
@@ -24,4 +37,9 @@ const items = ref([
   ul
     li(v-for="item in items")
       <ItemInfo :item="item" />
+  hr
+  h1 Currently Skilling:
+  <SkillInfo :skill="current_skill_target" />
+  h1(class="bg-purple-500") Inventory
+  p {{ inventory }}
 </template>
