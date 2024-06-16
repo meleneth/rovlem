d50a3a05fc776c8fdcc1a9d32e0c3b525143a6c8
Meleneth <meleneth@gmail.com>
2024-06-16 01:44:30 +0000

    [refactor] Item and Skills both exist as Pinia data stores now

---
+++ src/stores/skill.ts
@@ -0,0 +1,23 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useSkillStore = defineStore('skills', () => {
+  const skill_definitions = ref([
+    { name: 'Fishing', xp: 0, level: 1, maxlevel: 99, description: "catch raw fish"},
+    { name: 'Cooking', xp: 0, level: 1, maxlevel: 99, description: "Cook raw food, increasing the amount healed"}
+  ])
+  
+  const skills = ref({})
+  skill_definitions.value.forEach((s) => skills[s.name] = s)
+
+  function gain_skill_xp(skill_name, xp) {
+    const skill = skills[skill_name]
+    skill.xp = skill.xp + xp
+    if(skill.xp > (skill.level * 10)) {
+      skill.level++
+      console.log(`Gained level ${skill.level} in ${skill.name}`)
+    }
+  }
+
+  return { skills, skill_definitions, gain_skill_xp }
+})
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,54 +1,46 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { useItemStore } from '@/stores/item'
+import { useSkillStore } from '@/stores/skill'
+import { storeToRefs } from 'pinia'
 
 import ItemInfo from '../components/ItemInfo.vue'
 import SkillInfo from '../components/SkillInfo.vue'
 
-const skills = ref([
-  { name: 'Fishing', xp: 0, level: 1, maxlevel: 99, description: "catch raw fish"},
-  { name: 'Cooking', xp: 0, level: 1, maxlevel: 99, description: "Cook raw food, increasing the amount healed"}
-])
-
 const inventory = ref({})
 
-const items = ref([
-  {name: 'Raw Salmon', xp: {fishing: 5}, description: 'a tasty fish', ticks: 10},
-  {name: 'Raw Minnow', xp: {fishing: 1}, description: 'barely worth catching', ticks: 4},
-  {name: 'Raw Shrimp', xp: {fishing: 3}, description: 'delicious with butter', ticks: 7}
-]);
+const item_store = useItemStore()
+const { items } = storeToRefs(item_store)
 
-function gain_skill_xp(skill, xp) {
-  skill.xp = skill.xp + xp
-  if(skill.xp > (skill.level * 10)) {
-    skill.level = skill.level + 1
-  }
-}
+const skill_store = useSkillStore()
+const { skills, skill_definitions, gain_skill_xp } = storeToRefs(skill_store)
+
+const current_skill_target = items.value[0]
+
+const current_skill = skills['Fishing']
 
 var num_ticks = ref(0);
+
 const ticktimer = setInterval(() => {
   num_ticks.value = num_ticks.value + 1;
   if (num_ticks.value > current_skill_target.ticks) {
     num_ticks.value -= current_skill_target.ticks
     inventory.value[current_skill_target.name] = (inventory.value[current_skill_target.name] || 0) + 1
-    gain_skill_xp(current_skill, current_skill_target.xp.fishing)
+    skill_store.gain_skill_xp(current_skill.name, current_skill_target.xp.fishing)
   }
 }, 100);
-
-const current_skill_target = items.value[0]
-const current_skill = skills.value[0]
-
 </script>
 
 <template lang="pug">
   ul
-    li(v-for="skill in skills")
+    li(v-for="skill in skill_definitions")
       <SkillInfo :skill="skill" />
   ul
     li(v-for="item in items")
       <ItemInfo :item="item" />
   hr
   h1 Currently Skilling:
-  <SkillInfo :skill="current_skill_target" />
+    <SkillInfo :skill="current_skill_target" />
   h1(class="bg-purple-500") Inventory
   table
     tr(v-for="(qty, name) of inventory")
