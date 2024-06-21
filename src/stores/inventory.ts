fce0e2bcd8239001febbba150ef3e18138576019
Meleneth <meleneth@gmail.com>
2024-06-21 09:51:01 +0000

    [typefix] remove this from inventory pinia store

--- src/stores/inventory.ts
+++ src/stores/inventory.ts
@@ -8,7 +8,7 @@ export const useInventoryStore = defineStore('inventory', () => {
 
   function add_items(item_name: string, qty: number) {
     const new_qty = (inventory[item_name] || 0) + qty
-    this.inventory[item_name] = new_qty
+    inventory[item_name] = new_qty
   }
 
   function consume_items(item_name: string, qty: number) {
@@ -17,7 +17,7 @@ export const useInventoryStore = defineStore('inventory', () => {
     if(new_qty < 0) {
       throw "Not enough"
     }
-    this.inventory[item_name] = new_qty
+    inventory[item_name] = new_qty
   }
 
   return { inventory, add_items, consume_items }
